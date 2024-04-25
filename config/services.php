df414f780a527fabdd5f7a1c6fc12c458711b7d7
NujanSitaula <nujan.sitaula@outlook.com>
2024-04-25 11:53:14 +0545

    Xero connection bug fixed.

--- config/services.php
+++ config/services.php
@@ -18,6 +18,12 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    'xero' => [
+        'client_id' => env('XERO_CLIENT_ID'),
+        'client_secret' => env('XERO_CLIENT_SECRET'),
+        'redirect_uri' => env('XERO_REDIRECT_URI'),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
