f64f8462b957c205bd54a3c2c262a191ed788fba
AdarshGhimire1 <102452420+AdarshGhimire1@users.noreply.github.com>
2024-05-03 11:50:13 +0545

    fix: Migration

--- app/Http/Requests/DeliveryRequest.php
+++ app/Http/Requests/DeliveryRequest.php
@@ -28,23 +28,24 @@ class DeliveryRequest extends FormRequest
     {
         // Common rules for both 'post' and 'put'/'patch' methods
         $rules = [
-            'schedule_id' => 'exists:schedules,id',
-            'vehicle_id' => [Rule::exists('assets', 'id')->where('asset_type', 'vehicle')],
-            'driver_id' => [$this->roleRule('driver')],
-            'materials' => 'array',
-            'amount_loaded' => 'array',
-            'status' => 'in:pending,in_progress,completed',
-            'trip_number' => 'string',
-            'attachment' => ['nullable', 'array', 'mimes:pdf,doc,docx,txt,png,jpg,jpeg'],
+            'schedule_id' => 'sometimes|exists:schedules,id',
+            'vehicle_id' => ['sometimes', Rule::exists('assets', 'id')->where('asset_type', 'vehicle')],
+            'driver_id' => ['sometimes', $this->roleRule('driver')],
+            'materials' => 'sometimes|array',
+            'amount_loaded' => 'sometimes|array',
+            'status' => 'sometimes|required|in:pending,in_progress,completed',
+            'trip_number' => 'sometimes|string',
+            'attachment' => ['nullable', 'array'],
+            'notes' => 'nullable|string',
         ];
 
         if ($this->isMethod('post')) {
             // Additional rules for 'post' method
-            $rules['schedule_id'] = 'required|'.$rules['schedule_id'];
-            $rules['materials'] = 'required|'.$rules['materials'];
-            $rules['amount_loaded'] = 'required|'.$rules['amount_loaded'];
-            $rules['status'] = 'required|'.$rules['status'];
-            $rules['trip_number'] = 'required|'.$rules['trip_number'];
+            $rules['schedule_id'] = 'required|' . $rules['schedule_id'];
+            $rules['materials'] = 'required|' . $rules['materials'];
+            $rules['amount_loaded'] = 'required|' . $rules['amount_loaded'];
+            $rules['status'] = 'required|' . $rules['status'];
+            $rules['trip_number'] = 'required|' . $rules['trip_number'];
         }
 
         return $rules;
