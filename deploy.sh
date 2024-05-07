ac489b688fbb1cb3c3eaeeb5c7ede074af6185b2
Ubuntu <smcuser@smcdemo.iht4trauveeulapxrpuuseayvb.rx.internal.cloudapp.net>
2024-05-07 10:21:12 +0000

    changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ sudo apt update
 /usr/local/bin/composer install
 /usr/local/bin/composer update
 /usr/local/bin/composer upgrade
-php artisan key:generate
-php artisan migrate:fresh 
-php artisan db:seed
+sudo php artisan key:generate
+sudo php artisan migrate:fresh 
+sudo php artisan db:seed
 rm -rf public/storage
-php artisan storage:link
+sudo php artisan storage:link
 sudo php artisan serve --port=8000 &
